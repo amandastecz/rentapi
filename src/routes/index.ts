3f4c1ff0ce53e676c154c890a2fce978d66fb59b
amandastecz <amandasstecz@gmail.com>
2022-06-12 01:24:56 -0300

    feat(api): created user's controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specification.route";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 // Passando a route dessa forma, significa que todas os routers categories terão essa rota
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
-
+router.use("/users", usersRoutes)
 
 export {router};
