fb2cc6f01e91ccbd42d93fc1e93dcb00e5964520
amandastecz <amandasstecz@gmail.com>
2022-06-13 22:58:02 -0300

    feat(api): added bcrypt to crypt password

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { hash } from "bcryptjs";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO"
 
@@ -10,9 +11,19 @@ class CreateUserUseCase{
     ){};
 
     async execute({name, password, email, driver_license}: ICreateUserDTO): Promise<void>{
+
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if(userAlreadyExists){
+            throw new Error("Email already exists!");
+            
+        }
+
+        const passwordHash = await hash(password, 8);
+
         await this.usersRepository.create({
             name,
-            password,
+            password: passwordHash,
             email,
             driver_license
         })
