c7f94f59dc83f4d8c488e661669e914c76ec6315
amandastecz <amandasstecz@gmail.com>
2022-06-13 23:56:02 -0300

    fin(api): created JWT authentication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+
+import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specification.route";
 import { usersRoutes } from "./users.routes";
@@ -8,6 +10,7 @@ const router = Router();
 // Passando a route dessa forma, significa que todas os routers categories terão essa rota
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
-router.use("/users", usersRoutes)
+router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export {router};
