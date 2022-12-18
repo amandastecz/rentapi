6b82f5a7af948f22441fe1bdb3979c959c61061b
amandastecz <amandasstecz@gmail.com>
2022-12-18 18:55:12 -0300

    test(cars): tdd with cars develop

--- src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,14 +1,25 @@
+import { CarsRepositoryInMemory } from "../../repositories/inMemory/CarsRepositoryInMemory";
 import { CreateCarUseCase } from "./CreateCarUseCase"
 
 let createCar: CreateCarUseCase;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 
 describe("Create Car", () => {
 
     beforeEach(() => {
-        createCar = new CreateCarUseCase();
+        carsRepositoryInMemory = new CarsRepositoryInMemory();
+        createCar = new CreateCarUseCase(carsRepositoryInMemory);
     })
 
     it("should be able to create a new car", async () => {
-        await createCar.execute();
+        await createCar.execute({
+            name: "Mercedes Benz",
+            description: "GLC 300",
+            daily_rate: 100,
+            license_plate: "ABC",
+            fine_amount: 60,
+            brand: "Brand",
+            category_id: "category"
+        });
     })
 })
--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,8 +1,25 @@
-import { injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
+import { ICarsRepository } from "../../repositories/ICarsRepository";
 
-@injectable()
+interface IRequest {
+    name: string;
+    description: string;
+    daily_rate: number;
+    license_plate: string;
+    fine_amount: number;
+    brand: string;
+    category_id: string;
+}
+
+//@injectable()
 class CreateCarUseCase {
-    async execute(): Promise<void> {
+
+    constructor(
+       // @inject("CarsRepository")
+        private carsRepository: ICarsRepository
+    ){}
+
+    async execute({ name, description, daily_rate,license_plate,fine_amount, brand, category_id} : IRequest): Promise<void> {
 
     }
 }
