cfae37033bfc77ab6713669673b4279c13aa482b
amandastecz <amandasstecz@gmail.com>
2022-12-18 22:56:54 -0300

    feat(cars): created CarsRepository

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,22 +1,47 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
+import { Category } from "./Category";
 
+@Entity("cars")
 class Car {
-    id: string;
+
+    @PrimaryColumn()
+    id?: string;
+
+    @Column()
     name: string;
+
+    @Column()
     description: string;
+
+    @Column()
     daily_rate: number;
-    available: boolean;
+
+    @Column()
+    available = true;
+
+    @Column()
     license_plate: string;
+
+    @Column()
     fine_amount: number;
+
+    @Column()
     brand: string;
+
+    @Column()
     category_id: string;
+
+    @ManyToOne(() => Category)
+    @JoinColumn({name: "category_id"})
+    category: Category;
+
+    @CreateDateColumn()
     created_at: Date;
 
     constructor(){
         if(!this.id){
             this.id = uuidv4();
-            this.available = true;
-            this.created_at = new Date();
         }
     }
 }
