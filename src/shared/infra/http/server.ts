6c38f12d67995ad6f45590218b2f7025e88e89c0
amandastecz <amandasstecz@gmail.com>
2023-01-07 02:40:51 -0300

    feat(database): seed admin with create connection in db

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,13 +2,15 @@ import express, { NextFunction, Request, Response } from "express"
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express"
 
-import "../typeorm"
+import { createConnection } from "typeorm";
 
 import "../../container"
 
 import { router } from "./routes"
 import swaggerFile from "../../../swagger.json"
 import { AppError } from "../../errors/AppError"
+
+createConnection();
 
 const app = express();
 
---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from "uuid";
+import { hash } from "bcryptjs";
+import createConnection from "../index";
+
+async function create(){
+    const connection = await createConnection("localhost");
+
+    const id = uuidv4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+        `INSERT INTO USERS(id, name, email, password, driver_license, "isAdmin", created_at)
+        values(
+            '${id}', 'admin', 'admin@rentapi.com.br', 'ABC5577', '${password}', true, 'now()'
+        )`
+    );
+
+    await connection.close();
+}
+
+create().then(() => console.log("user admin created by seed"));
