1d22d4087b7c3761ecc4eacb11f75ba9fc282937
amandastecz <amandasstecz@gmail.com>
2023-01-17 00:51:47 -0300

    feat(admin): categories and specification routes with ensure admin and auth middlewares

--- src/shared/infra/http/routes/specification.route.ts
+++ src/shared/infra/http/routes/specification.route.ts
@@ -1,13 +1,14 @@
 import {Router } from "express";
 
-import { ensureAuthenticated } from "../middlewares/ensuredAuthenticated";
+import { ensureAuthenticated, } from "../middlewares/ensuredAuthenticated";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
+
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController()
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSpecificationController.handle);
+specificationsRoutes.post("/", ensureAuthenticated, ensureAdmin, createSpecificationController.handle);
 
 export {specificationsRoutes};  
