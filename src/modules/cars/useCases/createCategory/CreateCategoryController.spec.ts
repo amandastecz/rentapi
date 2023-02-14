394d7a7169971fab6365cdd223f87a599ac4a086
amandastecz <amandasstecz@gmail.com>
2023-02-14 00:33:47 -0300

    config(node-env) = test

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -2,7 +2,13 @@ import request from "supertest";
 import { app } from "../../../../shared/infra/http/app";
 
 describe("Create Category Controller", () => {
-    it("hello world!", async()=>{
-        await request(app).get("/cars/available").expect(200);
+    it("should be able to create a new category", async()=>{
+        const response = await request(app).post("/categories")
+        .send({
+            "name": "Category Supertest",
+            "description": "integration test"
+        });
+
+        expect(response.status).toBe(201);
     })
 })
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -2,11 +2,13 @@ import { createConnection, getConnectionOptions } from 'typeorm';
 
 interface IOptions {
   host: string;
+  database: any;
 }
 
 getConnectionOptions().then(options => {
   const newOptions = options as IOptions;
-  newOptions.host = 'database'; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  newOptions.host = process.env.NODE_ENV === "test" ? "localhost" : "database"; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  newOptions.database = process.env.NODE_ENV === "test" ? "rentx_test": options.database;
   createConnection({
     ...options,
   });
