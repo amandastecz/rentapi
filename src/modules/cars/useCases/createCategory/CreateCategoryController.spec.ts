08bca8e804f3a3e88125a8055eb175214021a1c1
amandastecz <amandasstecz@gmail.com>
2023-08-01 01:05:34 -0300

    fix(test):  Category Controller

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -35,14 +35,14 @@ describe("Create Category Controller", () => {
             password: "123456"
         });
 
-        const { token } = responseToken.body;
+        const { refresh_token} = responseToken.body;
 
         const response = await request(app).post("/categories")
         .send({
             "name": "Category Supertest",
             "description": "integration test"
         }).set({
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${refresh_token}`,
         });
 
         expect(response.status).toBe(201);
@@ -54,14 +54,14 @@ describe("Create Category Controller", () => {
             password: "123456"
         });
 
-        const { token } = responseToken.body;
+        const { refresh_token} = responseToken.body;
 
         const response = await request(app).post("/categories")
         .send({
             "name": "Category Supertest",
             "description": "integration test"
         }).set({
-            Authorization: `Bearer ${token}`,
+            Authorization: `Bearer ${refresh_token}`,
         });
 
         expect(response.status).toBe(400);
