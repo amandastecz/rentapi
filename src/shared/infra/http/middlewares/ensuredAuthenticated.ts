24f111cec5be9edf2e1027de2c93c551728147fb
amandastecz <amandasstecz@gmail.com>
2023-08-04 20:17:16 -0300

    refactor(): refresh token

--- src/shared/infra/http/middlewares/ensuredAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensuredAuthenticated.ts
@@ -11,29 +11,17 @@ interface IPayload {
 
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
     const authHeader = request.headers.authorization;
-    const userTokensRepository = new UserTokensRepository();
-
     if(!authHeader){
         throw new AppError("Token missing", 401);   
     }
-
     const [, token] = authHeader.split(" ");
-
     try {
-        const { sub: user_id } = verify(token, auth.secret_refresh_token ) as IPayload;
-        const user = await userTokensRepository.findByUserIdAndRefreshToken(user_id, token);
-        if(!user){
-            throw new AppError("User doesn't exists!", 401);
-            
-        }
-
+        const { sub: user_id } = verify(token, auth.secret_token ) as IPayload;
         request.user = {
             id: user_id
         }
-
         next();
     } catch (error) {
         throw new AppError("Token invalid!", 401);
-        
     }
 }
