801f50b9813192268f4c015b8767c642505a9146
amandastecz <amandasstecz@gmail.com>
2023-08-05 14:28:59 -0300

    fix(): await fs.promises

--- src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/S3StorageProvider.ts
@@ -15,7 +15,7 @@ class S3StorageProvider implements IStorageProvider{
     
     async save(file: string, folder: string): Promise<string> {
         const originalName = resolve(upload.tmpFolder,file);
-        const fileContent = fs.promises.readFile(originalName);
+        const fileContent = await fs.promises.readFile(originalName);
         const contentType = mime.getType(originalName);
         await this.client
             .putObject({
