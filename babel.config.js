c1696e77284883a44ca60e7b697f77a657eaa173
amandastecz <amandasstecz@gmail.com>
2023-08-08 23:54:57 -0300

    chore(): babel config

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-    presets: [],
-    plugins: []
+    presets: [
+    ["@babel/preset-env", { target: {node: "current"}}],
+    "@babel/preset-typescript",
+    ],
+    plugins: [
+        "babel-plugin-transform-typescript-metadata",
+        ["@babel/plugin-proposal-decorators", { legacy: true}],
+        ["@babel/plugin-proposal-class-properties", { loose: true}],
+    ]
 }
