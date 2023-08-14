7dda37c6aba23a738a16fe16f393f87d6bbae345
amandastecz <amandasstecz@gmail.com>
2023-08-13 22:56:52 -0300

    feat(): RateLimiterRedis middleware

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import { createClient } from "redis";
+import { AppError } from "../../../../shared/errors/AppError";
+
+const redisClient = createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+});
+
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "rateLimiter",
+  points: 5, // 5 requests
+  duration: 5, // per 5 seconds by IP
+});
+
+export default async function rateLimiter(request: Request, response: Response, next: NextFunction): Promise<void> {
+  try {
+    await limiter.consume(request.ip);
+
+    return next();
+  } catch (err) {
+    throw new AppError(`Too many requests ---> ${err}`, 429);
+  }
+}
